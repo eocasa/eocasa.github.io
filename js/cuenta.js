7a38438c3da3fc6ac973ba4f4296fb73e8d192ea
eocasa <epetnpolonia@gmail.com>
2025-08-21 20:55:20 -0300

    Add files via upload

--- js/cuenta.js
+++ js/cuenta.js
@@ -31,5 +31,5 @@ const getRemainingTime = deadline => {
     }, 1000)
   };
   
-  countdown('Oct 1 2024 7:00:00 GMT-0300', 'cuenta', 'Redirigiendo...');
+  countdown('OCT 1 2025 7:00:00 GMT-0300', 'cuenta', 'Redirigiendo...');
   
